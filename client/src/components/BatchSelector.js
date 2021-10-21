6ed742420abba1fba1483db5cde57d498be73fd8
Yashasvi Sinha <yashasvi.kumar.sinha@gmail.com>
2021-10-21 19:52:51 +0530

    improved card design

--- client/src/components/BatchSelector.js
+++ client/src/components/BatchSelector.js
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect } from 'react'
-import { Card, Container } from 'react-bootstrap'
 import { withRouter } from 'react-router'
 import AppContext from '../context/AppContext'
 import { getAllBatch } from '../queries/service'
+import { Card, Col, Row } from "antd";
+import Meta from 'antd/lib/card/Meta';
 
 function BatchSelector(props) {
 
@@ -14,14 +15,14 @@ function BatchSelector(props) {
 
             if (Array.isArray(batches)) {
                 setBatches([...batches])
-                setActiveBatch(batches[0].batch_id)    
-            }else {
+                setActiveBatch(batches[0].batch_id)
+            } else {
 
                 props.history.push('/login')
 
             }
 
-            
+
         })
 
     }, [setBatches, setActiveBatch, props.history])
@@ -29,24 +30,19 @@ function BatchSelector(props) {
 
 
     return (
-        <Container className="d-flex justify-content-center">
+        <Row gutter={16} className='my-2'>
             {batches.map(batch => {
-                const {batch_id: batchId, name} = batch
-
+                const { batch_id: batchId, name } = batch
                 return (
-                    <Card onClick={() => setActiveBatch(batchId)} key={batchId} className={`shadow mx-3 ${activeBatch === batchId ? 'active-batch text-white': '' } `}>
-                        <Card.Body>
-                            <Card.Title>{name}</Card.Title>
-                            <Card.Text>
-                                Batch ID: {batchId}
-                            </Card.Text>
-                            {/* <Button variant="primary">Go somewhere</Button> */}
-                        </Card.Body>
-                    </Card>
+                    <Col>
+                        <Card hoverable onClick={() => setActiveBatch(batchId)} className={`${activeBatch === batchId ? 'active-batch' : ''} `} key={batchId}>
+                            <Meta title={name} description={`Batch Id: ${batchId}`} />
+                        </Card>
+                    </Col>
                 )
 
             })}
-        </Container>
+        </Row>
     )
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,7 +16,6 @@ function Home() {
 
   return (
     <div className="Home">
-      <h1>Home page</h1>
       <BatchSelector />
       <FeedbackReport />
     </div>
